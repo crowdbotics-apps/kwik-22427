c2015576457756ce4b4bb731f670bf6f3ea92b3d
Crowdbotics <team@crowdbotics.com>
2020-11-09 03:36:57 +0000

    Update models

--- backend/kwik_22427/wsgi.py
+++ backend/kwik_22427/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kwik_22427.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kwik_22427.settings")
 
 application = get_wsgi_application()
